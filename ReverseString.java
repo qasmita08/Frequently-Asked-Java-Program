4983f6fcdda90b63b03d35aaad7ed61fba4e96e5
qasmita08 <126377597+qasmita08@users.noreply.github.com>
2023-02-27 19:42:59 +0530

    Add files via upload

---
+++ ReverseString.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+		
+		Scanner sc;
+		String rev= "";
+		
+		sc = new Scanner(System.in);
+		 
+		System.out.println("Enter the String");
+		String input  = sc.next();
+		
+		//Using for Loop and Concatenation
+		
+		int length = input.length();
+		
+		/*for(int i=length-1;i>=0;i--) {
+			
+			rev = rev + input.charAt(i);
+			
+		}*/
+		
+		
+		// Using charArrray
+		
+		/*char[] arr = input.toCharArray();
+		
+		for(int i = length-1; i>=0;i--) {
+			
+			rev = rev + arr[i];
+		}*/
+		
+		//Using StringBuilder Class
+		
+		StringBuilder sb = new StringBuilder(input);
+		System.out.println("Reverse String is " + sb.reverse());
+		
+		
+
+	}
+
+}
